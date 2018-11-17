518891141efe25dc34a2db72dfad9826e196bb62
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2018-11-17 08:46:47 -0200

    Refatorando e terminando testes

--- src/app/fizzbuzz.js
+++ src/app/fizzbuzz.js
@@ -1,8 +1,7 @@
-const FizzBuzz = (num) => {
-  if(num % 3 === 0 && num % 5 === 0) return `FizzBuzz`;
-  if(num % 3 === 0) return `Fizz`;
-  if(num % 5 === 0) return `Buzz`;
+exports.FizzBuzz = function(num) {
+    if(num % 3 === 0 && num % 5 === 0) return `FizzBuzz`;
+    if(num % 3 === 0) return `Fizz`;
+    if(num % 5 === 0) return `Buzz`;
 
-  return num;
+    return num;
 };
-export { FizzBuzz };
--- src/test/fizzbuzz-test.js
+++ src/test/fizzbuzz-test.js
@@ -10,16 +10,26 @@ Escreva uma lib que receba um número e:
 */
 const chai = require('chai');
 const fizzbuzz = require('../app/fizzbuzz');
-var assert = chai.assert;
+var expect = chai.expect;
 
 describe('FizzBuzz', () => {
 
   it('Teste: Se numero é divisivel por 3 retornar Fizz', () => {
+      expect(fizzbuzz.FizzBuzz(6)).to.be.equal(`Fizz`);
+      expect(fizzbuzz.FizzBuzz(6)).to.be.equal(`Fizz`);
   });
   it('Teste: Se numero é divisivel por 5 retornar Buzz', () => {
+    expect(fizzbuzz.FizzBuzz(10)).to.be.equal(`Buzz`);
+    expect(fizzbuzz.FizzBuzz(25)).to.be.equal(`Buzz`);
   });
   it('Teste: Se numero é divisivel por 3 e 5 retornar FizzBuzz', () => {
+    expect(fizzbuzz.FizzBuzz(30)).to.be.equal(`FizzBuzz`);
+    expect(fizzbuzz.FizzBuzz(60)).to.be.equal(`FizzBuzz`);
   });
   it('Teste: Se numero não é divisivel por 3 e 5 retornar numero', () => {
+    expect(fizzbuzz.FizzBuzz(7)).to.be.equal(7);
+    expect(fizzbuzz.FizzBuzz(14)).to.be.equal(14);
+
+
   });
 });
